6ba5eb9b697f85ca9127d1fe0939395d0dec2dd4
Linwei Tao <dylan.tao94@gmail.com>
2025-04-30 20:13:27 +1000

    add auroc

--- simple_evals.py
+++ simple_evals.py
@@ -55,7 +55,7 @@ def main():
 
     simpleqa_grader = all_models["meta-llama/Llama-3.3-70B-Instruct-Turbo-Free"]
     equality_checker = all_models["meta-llama/Llama-3.3-70B-Instruct-Turbo-Free"]
-    decisiveness_grader = all_models["meta-llama/Llama-3.3-70B-Instruct-Turbo-Free"]
+    decisiveness_grader = all_models["gpt-4.1-nano"]
 
 
     # ^^^ used for fuzzy matching, just for math
