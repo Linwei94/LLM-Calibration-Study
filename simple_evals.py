95d3dab52d1b9624d2fb387c8454291ad5191e4c
Linwei Tao <dylan.tao94@gmail.com>
2025-05-01 11:37:32 +1000

    new decisiveness eval for GPT-4.1-mini and GPT-4.1-nano

--- simple_evals.py
+++ simple_evals.py
@@ -55,7 +55,7 @@ def main():
 
     simpleqa_grader = all_models["meta-llama/Llama-3.3-70B-Instruct-Turbo-Free"]
     equality_checker = all_models["meta-llama/Llama-3.3-70B-Instruct-Turbo-Free"]
-    decisiveness_grader = all_models["gpt-4.1-nano"]
+    decisiveness_grader = all_models["gpt-4.1-mini"]
 
 
     # ^^^ used for fuzzy matching, just for math
