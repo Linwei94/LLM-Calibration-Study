8df3f2e6aed8e2d1563b98fd3f2c28feca81ff49
Ivan-Yeh <ivan.hsuu@gmail.com>
2025-05-06 13:58:49 +1000

    updated batch downloader model list

--- batch_downloader.sh
+++ batch_downloader.sh
@@ -2,19 +2,39 @@
 
 # 定义要处理的模型列表（你可以自由添加）
 MODEL_LIST=(
+    Qwen/Qwen3-0.6B
+    Qwen/Qwen3-0.6B-FP8
+    Qwen/Qwen3-0.6B-Base
+    Qwen/Qwen3-1.7B-FP8
+    Qwen/Qwen3-1.7B
+    Qwen/Qwen3-1.7B-Base
+    Qwen/Qwen3-4B-FP8
+    Qwen/Qwen3-4B
+    Qwen/Qwen3-4B-Base
+    Qwen/Qwen3-8B-FP8
+    Qwen/Qwen3-8B
+    Qwen/Qwen3-8B-Base
+    Qwen/Qwen3-14B-FP8
+    Qwen/Qwen3-14B-Base
+    Qwen/Qwen3-14B
+    Qwen/Qwen3-30B-A3B-Base
+    Qwen/Qwen3-30B-A3B-FP8
+    Qwen/Qwen3-30B-A3B
+    Qwen/Qwen3-32B-FP8
+    Qwen/Qwen3-32B
     google/gemma-3-27b-it
-    meta-llama/Llama-3.1-70B-Instruct
-    meta-llama/Llama-2-70B-Instruct
-    Qwen/Qwen2.5-7B-Instruct
-    Qwen/Qwen2.5-14B-Instruct
-    Qwen/Qwen2.5-32B-Instruct
-    Qwen/Qwen2.5-32B-Instruct-GGUF
-    Qwen/Qwen2.5-32B-Instruct-GPTQ-Int4
-    Qwen/Qwen2.5-32B-Instruct-GPTQ-Int8
-    Qwen/Qwen2.5-72B-Instruct
-    Qwen/QwQ-32B-AWQ
-    Qwen/QwQ-32B-GGUF
-    Qwen/QwQ-32B
+    google/gemma-2-27b-it
+    google/gemma-2-9b-it
+    google/gemma-2b-it
+    google/gemma-3-1b-it
+    google/gemma-3-4b-it
+    google/gemma-3-12b-it
+    mistralai/Mixtral-8x7B-Instruct-v0.1
+    mistralai/Mixtral-8x7B-v0.1
+    mistralai/Mistral-7B-Instruct-v0.1
+    mistralai/Mistral-Small-3.1-24B-Base-2503
+    mistralai/Mistral-Large-Instruct-2411
+    mistralai/Mistral-Nemo-Instruct-2407
     deepseek-ai/DeepSeek-R1-Distill-Llama-70B
     deepseek-ai/DeepSeek-R1-Distill-Llama-8B
     deepseek-ai/DeepSeek-R1-Distill-Qwen-32B
@@ -27,7 +47,7 @@ for MODEL_NAME in "${MODEL_LIST[@]}"; do
   echo "================================================================="
 
   # 调用单个模型处理脚本
-  bash upload_model.sh "$MODEL_NAME"
+  bash upload_model.sh "$MODEL_NAME" 
 
   # 检查上一步是否成功
   if [ $? -ne 0 ]; then
